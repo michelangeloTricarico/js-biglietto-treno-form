4e2a14d8479a9c86f73787f12e935616f01fadd8
michelangeloTricarico <michelangelotricarico@gmail.com>
2026-03-19 18:19:34 +0100

    Project finalization

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const km_element = document.querySelector(".km-value") // km field
 const age_element = document.querySelector(".age-selection") // age field
 const submit_btn_element = document.querySelector(".btn-submit") // submit button
 const reset_btn_element = document.querySelector(".btn-reset") // reset button
+const form_element = document.querySelector("form") // reset button
 
 //Output
 let passenger_name_element = document.getElementById("name_surname_output") // Name and surname to compile
@@ -17,11 +18,12 @@ let code_num_element = document.getElementById("code_num") // Code CP to compile
 let cost_num_element = document.getElementById("cost_num") // Cost to compile
 
 //Listen to compile ticket connected to function for the elaboration
-submit_btn_element.addEventListener("submit", TiketCompilation)
-//reset_btn_element.addEventListener("reset", TiketReset)
+form_element.addEventListener("submit", TiketCompilation)
+form_element.addEventListener("reset", TiketReset)
 
-function TiketCompilation(){
-    console.log("qui")
+function TiketCompilation(event){
+    event.preventDefault() //not provvide submit
+    console.log("compile")
     let age_passeger = age_element.value
     let km_number = km_element.value
     let cost
@@ -40,15 +42,30 @@ function TiketCompilation(){
         offer="Biglietto standard"
     }
     // Load data in interface
-    let passenger_name = passenger_name_element.value
-    passenger_name_element.innerHTML(passenger_name)
-    offer_type_element.innerHTML(offer)
+    let passenger_name = name_surname_element.value
+    //console.log(passenger_name )
+    passenger_name_element.innerHTML = passenger_name
+    offer_type_element.innerHTML = offer 
     let cabinet=Math.floor(Math.random()*10000) // Take an int number for cabinet
-    cabinet_num_element.innerHTML(cabinet)
+    cabinet_num_element.innerHTML = cabinet
     let cod=Math.floor(Math.random()*100) // Take an int number for code CP
-    code_num_element.innerHTML(cod)
-    cost_num_element.innerHTML(cost+" "+"€")
+    code_num_element.innerHTML = cod 
+    cost_num_element.innerHTML = cost.toFixed(2)+" "+"€"
+    
+}
+
+// Function to delate field 
+function TiketReset(event){
     event.preventDefault() //not provvide submit
+    console.log("reset")
+    name_surname_element.value = ""
+    km_element.value = ""
+    age_element.value = 1
+    passenger_name_element.innerHTML = "Nome e Cognome"
+    offer_type_element.innerHTML = "None" 
+    cabinet_num_element.innerHTML = "None" 
+    code_num_element.innerHTML = "None" 
+    cost_num_element.innerHTML = "None" 
 }
 
 
